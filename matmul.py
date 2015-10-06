160032683805469eb79e90fc46c57abaa64f1b23
jkanukur <jkanukur@gmail.com>
2015-10-06 13:50:14 -0400

    'Matrix Addition and Multiplication in Lists'

--- matmul.py
+++ matmul.py
@@ -1,6 +1,13 @@
 A=[[1,2],[3,4]]
 B=[[5,6],[7,8]]
 
-C=A+B
+C=[[A[0][0]+B[0][0],A[0][1]+B[0][1]],[A[1][0]+B[1][0],A[1][1]+B[1][1]]]
 
+D = [[A[0][0]*B[0][0]+A[0][1]*B[1][0], A[0][0]*B[0][1]+A[0][1]*B[1][1]],\
+     [A[1][0]*B[0][0]+A[1][1]*B[1][0], A[1][0]*B[0][1]+A[1][1]*B[1][1]]]
+
+print "The sum of matrices A and B is: "
 print C
+
+print "The product of matrices A and B is: "
+print D
